d58467e0822f6260937973f37c9766d98665818b
Mat Holroyd <contact@matholroyd.com>
2014-11-09 21:15:55 +1100

    Added a background js, which borrows much of the code from popup.js

---
+++ background.js
@@ -0,0 +1,39 @@
+var EmailTools = function() {
+  var that = {};
+  
+  that.initialize = function() {
+    chrome.contextMenus.create({
+      "title": "Generate Email",
+      "contexts": ["editable"],
+      "onclick": that.onclickGenerateEmail
+    });
+  };
+
+  that.onclickGenerateEmail = function(info) {
+    that.getRandomEmail(function(email) {
+      // TODO set the value of the currently acrive editable thing to 'email'
+    });
+  };
+  
+  
+  that.getRandomEmail = function(callback) {
+    var url = "http://www.google.com",
+        xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+      // Replace this with the actual xhr response once API built
+      callback("some-random-email@tworgy.com");
+    }
+    xhr.send();
+  };
+    
+  that.displayEmailAddress = function(emailAddress) {
+    document.getElementById('email').innerText = emailAddress;
+  };
+  
+  return that;
+}();
+
+EmailTools.initialize();
+

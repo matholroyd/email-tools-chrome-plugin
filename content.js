c711712e89c672a6f71f6117ce6556a74b9b7218
Mat Holroyd <contact@matholroyd.com>
2014-11-10 20:06:02 +1100

    Added TODO comment how should probably not blindly insert email into current input

--- content.js
+++ content.js
@@ -2,11 +2,13 @@
 
   chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if(message.name === "msgGenerateEmail") {
+      // TODO should check the editable thing is actually something would want 
+      // an email address put into it
       document.activeElement.value = message.email;
     } else {
       console.log("Unexpected message!\n" + message);
     }
   });
-  
+    
 })();
 

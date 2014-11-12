a28ca9255fe7cd852a3c90c9c713f39eb8bada7d
Mat Holroyd <contact@matholroyd.com>
2014-11-12 16:21:09 +1100

    Refactored to using an object literal for Background.prototype

--- background/background.js
+++ background/background.js
@@ -25,67 +25,66 @@
     });
   }
 
-  Background.prototype.saveSettingsTosServer = function() {
-    chrome.storage.sync.get({
-      api_key: null,
-      username: null,
-      email: null
-    }, function(settings) {
-      if(settings.api_key !== null && settings.api_key !== undefined) {
-        var url = host + "/api/" + settings.api_key + "/settings",
-            xhr = new XMLHttpRequest();
+  Background.prototype = {
+    saveSettingsTosServer: function() {
+      chrome.storage.sync.get({
+        api_key: null,
+        username: null,
+        email: null
+      }, function(settings) {
+        if(settings.api_key !== null && settings.api_key !== undefined) {
+          var url = host + "/api/" + settings.api_key + "/settings",
+              xhr = new XMLHttpRequest();
 
-        xhr.open("PUT", url, true);
-        xhr.send(JSON.stringify(settings));
-      }
-    });
-  };
-  
-  Background.prototype.createNewAccount = function() {
-    var url = host + "/create-new-account",
-        xhr = new XMLHttpRequest();
+          xhr.open("PUT", url, true);
+          xhr.send(JSON.stringify(settings));
+        }
+      });
+    },
+    createNewAccount: function() {
+      var url = host + "/create-new-account",
+          xhr = new XMLHttpRequest();
 
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === READYSTATE_DONE) {
-        data = JSON.parse(xhr.responseText);
+      xhr.open("GET", url, true);
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState === READYSTATE_DONE) {
+          data = JSON.parse(xhr.responseText);
     
-        chrome.storage.sync.set({
-          api_key: data.api_key
-        });
+          chrome.storage.sync.set({
+            api_key: data.api_key
+          });
+        }
       }
-    }
-    xhr.send();
-  };
-  
-  Background.prototype.onclickGenerateEmail = function(info) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-      Background.prototype.getRandomEmail(function(email) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          name: "msgGenerateEmail",
-          email: email
+      xhr.send();
+    },
+    onclickGenerateEmail: function(info) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        Background.prototype.getRandomEmail(function(email) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            name: "msgGenerateEmail",
+            email: email
+          });
         });
       });
-    });
-  };
-  
-  Background.prototype.getRandomEmail = function(callback) {
-    chrome.storage.sync.get({
-      api_key: null
-    }, function(settings) {
-      if(settings.api_key !== null && settings.api_key !== undefined) {
-        var url = host + "/api/" + settings.api_key + "/generate-email",
-            xhr = new XMLHttpRequest();
+    },
+    getRandomEmail: function(callback) {
+      chrome.storage.sync.get({
+        api_key: null
+      }, function(settings) {
+        if(settings.api_key !== null && settings.api_key !== undefined) {
+          var url = host + "/api/" + settings.api_key + "/generate-email",
+              xhr = new XMLHttpRequest();
 
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = function() {
-          if(xhr.readyState === READYSTATE_DONE) {
-            callback(xhr.responseText);
+          xhr.open("GET", url, true);
+          xhr.onreadystatechange = function() {
+            if(xhr.readyState === READYSTATE_DONE) {
+              callback(xhr.responseText);
+            }
           }
+          xhr.send();
         }
-        xhr.send();
-      }
-    });
+      });
+    }
   };
 
   new Background();

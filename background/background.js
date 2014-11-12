c8fcd6d312c58e5a2db9a84938c38e71d418ef34
Mat Holroyd <contact@matholroyd.com>
2014-11-12 15:32:01 +1100

    Added explicit background.html file

--- background/background.js
+++ background/background.js
@@ -1,113 +1,108 @@
-var EmailTools = function() {
-  var that = {},
-      host = "http://localhost:8000",
-      READYSTATE_DONE = 4,
-      config = null,
-      isConfigured = false;
+define(function(require) {
+  var EmailTools = function() {
+    var that = {},
+        host = "http://localhost:8000",
+        READYSTATE_DONE = 4,
+        config = null;
   
-  that.initialize = function() {
-    chrome.contextMenus.create({
-      title: "Generate Email",
-      contexts: ["editable"],
-      onclick: that.onclickGenerateEmail
-    });
+    that.initialize = function() {
+      chrome.contextMenus.create({
+        title: "Generate Email",
+        contexts: ["editable"],
+        onclick: that.onclickGenerateEmail
+      });
 
-    chrome.storage.sync.get({
-      api_key: undefined,
-      username: undefined,
-      email: undefined
-    }, function(items) {
-      if(items.api_key === null || items.api_key === undefined) {
-        that.createNewAccount();
-      }
-    });
+      chrome.storage.sync.get(function(items) {
+        if(items.api_key === null || items.api_key === undefined) {
+          that.createNewAccount();
+        }
+      });
     
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-      if(message.name === "msgOptionsSaved") {
-        that.saveSettingsTosServer();
-      } else {
-        console.log("Unexpected message!\n" + message);
-      }
+      chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+        if(message.name === "msgOptionsSaved") {
+          that.saveSettingsTosServer();
+        } else {
+          console.log("Unexpected message!\n" + message);
+        }
       
-    });
+      });
     
-  };
+    };
   
-  that.saveSettingsTosServer = function() {
-    chrome.storage.sync.get({
-      api_key: null,
-      username: null,
-      email: null
-    }, function(settings) {
-      if(settings.api_key !== null && settings.api_key !== undefined) {
-        var url = host + "/api/" + settings.api_key + "/settings",
-            xhr = new XMLHttpRequest();
+    that.saveSettingsTosServer = function() {
+      chrome.storage.sync.get({
+        api_key: null,
+        username: null,
+        email: null
+      }, function(settings) {
+        if(settings.api_key !== null && settings.api_key !== undefined) {
+          var url = host + "/api/" + settings.api_key + "/settings",
+              xhr = new XMLHttpRequest();
     
-        xhr.open("PUT", url, true);
-        xhr.send(JSON.stringify(settings));
-      }
-    });
-  };
+          xhr.open("PUT", url, true);
+          xhr.send(JSON.stringify(settings));
+        }
+      });
+    };
    
-  // that.loadSettings = function() {
-  //   chrome.storage.sync.get({
-  //     api_key: null
-  //   }, function(items) {
-  //     config = items;
-  //   });
-  // };
+    // that.loadSettings = function() {
+    //   chrome.storage.sync.get({
+    //     api_key: null
+    //   }, function(items) {
+    //     config = items;
+    //   });
+    // };
   
-  that.createNewAccount = function() {
-    var url = host + "/create-new-account",
-        xhr = new XMLHttpRequest();
+    that.createNewAccount = function() {
+      var url = host + "/create-new-account",
+          xhr = new XMLHttpRequest();
     
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === READYSTATE_DONE) {
-        data = JSON.parse(xhr.responseText);
+      xhr.open("GET", url, true);
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState === READYSTATE_DONE) {
+          data = JSON.parse(xhr.responseText);
         
-        chrome.storage.sync.set({
-          api_key: data.api_key
-        });
+          chrome.storage.sync.set({
+            api_key: data.api_key
+          });
+        }
       }
-    }
-    xhr.send();
-  };
+      xhr.send();
+    };
 
-  that.onclickGenerateEmail = function(info) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-      that.getRandomEmail(function(email) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          name: "msgGenerateEmail",
-          email: email
+    that.onclickGenerateEmail = function(info) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        that.getRandomEmail(function(email) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            name: "msgGenerateEmail",
+            email: email
+          });
         });
       });
-    });
-  };
+    };
   
   
-  that.getRandomEmail = function(callback) {
-    chrome.storage.sync.get({
-      api_key: null
-    }, function(settings) {
-      if(settings.api_key !== null && settings.api_key !== undefined) {
-        var url = host + "/api/" + settings.api_key + "/generate-email",
-            xhr = new XMLHttpRequest();
+    that.getRandomEmail = function(callback) {
+      chrome.storage.sync.get({
+        api_key: null
+      }, function(settings) {
+        if(settings.api_key !== null && settings.api_key !== undefined) {
+          var url = host + "/api/" + settings.api_key + "/generate-email",
+              xhr = new XMLHttpRequest();
     
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = function() {
-          if(xhr.readyState === READYSTATE_DONE) {
-            callback(xhr.responseText);
+          xhr.open("GET", url, true);
+          xhr.onreadystatechange = function() {
+            if(xhr.readyState === READYSTATE_DONE) {
+              callback(xhr.responseText);
+            }
           }
+          xhr.send();
         }
-        xhr.send();
-      }
-    });
-  };
+      });
+    };
       
-  return that;
-}();
+    return that;
+  }();
 
-EmailTools.initialize();
-
-
+  EmailTools.initialize();
+});
